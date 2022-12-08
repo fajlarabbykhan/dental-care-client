343911021aada0abf64c97ce5ef2f49aa0fa817c
Fajla Rabby Khan <fajlarabbyk55@gmail.com>
2022-12-08 15:06:20 +0600

    working on auth

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -7,13 +7,13 @@ const AvailableAppointments = ({ date }) => {
   const [services, setServices] = useState([]);
   const [treatment, setTreatment] = useState(null);
   useEffect(() => {
-    fetch("services.json")
+    fetch("http://localhost:5000/service")
       .then((res) => res.json())
       .then((data) => setServices(data));
   }, []);
   return (
     <div className="px-12">
-      <p className="text-center text-xl text-primary mb-5">
+      <p className="text-center text-xl text-primary mb-12">
         You picked Appointment for {format(date, "PP")}.
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -4,8 +4,8 @@ const Service = ({ service, setTreatment }) => {
   const { name, slots } = service;
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title text-secondary">{name}</h2>
+      <div className="card-body text-center">
+        <h2 className="text-secondary text-xl font-bold">{name}</h2>
         <p>
           {slots.length ? (
             <span>{slots[0]}</span>
@@ -21,7 +21,7 @@ const Service = ({ service, setTreatment }) => {
             disabled={slots.length == 0}
             onClick={() => setTreatment(service)}
             htmlFor="booking-modal"
-            className="btn btn-primary text-white font-bold bg-gradient-to-r from-[#5651e5] to-[#709dff]"
+            className="btn btn-sm btn-primary text-white font-bold bg-gradient-to-r from-[#5651e5] to-[#709dff]"
           >
             Book Appointment
           </label>
