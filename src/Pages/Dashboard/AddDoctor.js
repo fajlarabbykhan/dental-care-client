a84c472df333b1b394fe0a724ee9fbdbf2ef70c9
Fajla Rabby Khan <fajlarabbyk55@gmail.com>
2022-12-14 17:14:48 +0600

    Succesfully add a doctor in mondodb done with success message

--- src/Pages/Dashboard/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor.js
@@ -1,16 +1,72 @@
 import React from "react";
 
 import { useForm } from "react-hook-form";
+import { useQuery } from "react-query";
+import { toast } from "react-toastify";
+import Loading from "../Shared/Loading";
 
 const AddDoctor = () => {
   const {
     register,
     formState: { errors },
     handleSubmit,
+    reset,
   } = useForm();
+
+  const { data: services, isLoading } = useQuery("services", () =>
+    fetch("http://localhost:5000/service").then((res) => res.json())
+  );
+  const imageApi = "4494498d5b5648221b45ca160d56dd7f";
   const onSubmit = async (data) => {
-    console.log(data);
+    // console.log(data);
+    const formData = new FormData();
+
+    const image = data.image[0];
+    // console.log(image);
+    formData.append("image", image);
+    const url = `https://api.imgbb.com/1/upload?key=${imageApi}`;
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.success) {
+          const img = result.data.url;
+          const doctor = {
+            name: data.name,
+            email: data.email,
+            treatmentArea: data.treatmentArea,
+            img: img,
+          };
+          //   console.log("Doctor Info", doctor);
+          // post doctor data to data base
+          fetch("http://localhost:5000/doctor", {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+              authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+            body: JSON.stringify(doctor),
+          })
+            .then((res) => res.json())
+            .then((inserted) => {
+              //   console.log("Doctor info from server", inserted);
+              if (inserted.insertedId) {
+                toast.success("You add a doctor succesfully");
+                reset();
+              } else {
+                toast.error("You are not able to add a doctor.");
+              }
+            });
+        }
+        // console.log("Doctor Photo", result);
+      });
   };
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div className="mt-8">
       <p className="text-2xl">Add a new doctor</p>
@@ -81,7 +137,19 @@ const AddDoctor = () => {
           <label className="label">
             <span className="label-text">Treatment Area</span>
           </label>
-          <input
+
+          <select
+            className="select w-full max-w-xs input-bordered"
+            {...register("treatmentArea")}
+          >
+            {services.map((service) => (
+              <option key={service._id} value={service.name}>
+                {service.name}
+              </option>
+            ))}
+          </select>
+
+          {/* <input
             type="text"
             placeholder="Doctor Specialty"
             className="input input-bordered w-full max-w-xs"
@@ -92,19 +160,36 @@ const AddDoctor = () => {
               },
             })}
           />
+          */}
+        </div>
+        <div className="form-control w-full max-w-xs">
           <label className="label">
-            {errors.password?.type === "required" && (
+            <span className="label-text">Upload Doctor Image</span>
+          </label>
+          <input
+            type="file"
+            className="input input-bordered w-full max-w-xs"
+            {...register("image", {
+              required: {
+                value: true,
+                message: "Image is required",
+              },
+            })}
+          />
+          <label className="label">
+            {errors.name?.type === "required" && (
               <p role="alert" className="text-red-500">
-                {errors.password.message}
+                {errors.image.message}
               </p>
             )}
-            {errors.password?.type === "minLength" && (
+            {errors.name?.type === "pattern" && (
               <p role="alert" className="text-red-500">
-                {errors.password.message}
+                {errors.name.message}
               </p>
             )}
           </label>
         </div>
+
         <input
           type="submit"
           value="Add a Doctor"
